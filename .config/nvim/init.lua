b08645b94be24b1ef31d1c088375b6278a382e03
Luis Soto <lsoto2@apple.com>
2025-09-22 17:53:10 -0700

    Adds neovim tmux clipboard support

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -199,6 +199,18 @@ vim.g.encoding = 'utf-8'
 vim.opt.conceallevel = 2
 vim.opt.concealcursor = 'n'
 
+vim.g.clipboard = {
+	name = 'OSC 52',
+	copy = {
+		['+'] = require('vim.ui.clipboard.osc52').copy '+',
+		['*'] = require('vim.ui.clipboard.osc52').copy '*',
+	},
+	paste = {
+		['+'] = require('vim.ui.clipboard.osc52').paste '+',
+		['*'] = require('vim.ui.clipboard.osc52').paste '*',
+	},
+}
+
 -- {{{ Return to last position in file
 vim.api.nvim_create_autocmd("BufRead", {
 
